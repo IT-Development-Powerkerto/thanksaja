37219e6bc2bfd06cddf31978fd7a66d47c0966a0
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-01-03 08:48:24 +0700

    Fix Thankspage

--- app/Http/Controllers/ThanksController.php
+++ app/Http/Controllers/ThanksController.php
@@ -7,9 +7,8 @@ use Illuminate\Http\Request;
 class ThanksController extends Controller
 {
     public function redirectWA($wa, $text){
-        // echo "<script>setTimeout(function(){ window.location.href = 'https://api.whatsapp.com/send/?phone='.$wa; }, 3000);</script>";
         $wa = $wa;
         $text = $text;
-        return view('welcome', compact('wa'), compact('text'));
+        return view('thanks', compact('wa'), compact('text'));
     }
 }
--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -38,6 +38,7 @@
             <img src="../img/pana.png" alt="Terima Kasih">
         </div>
     </div>
+    <script>setTimeout(function(){ window.location.href = 'https://api.whatsapp.com/send/?phone={{ $wa }}&text={{ $text }}'; }, 3000);</script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
